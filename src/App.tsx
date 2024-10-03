0fda883ddb8cde3b0029f21a30d69b85f074b4e6
muhammad sahad <muhammadsahad2022@gmail.com>
2024-10-04 00:41:11 +0530

    chore : remove unuserd React

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import HomePage from "./page/HomePage";
 import { Route, Routes, BrowserRouter as Router, useLocation } from "react-router-dom";
 import LoginPage from "./page/LoginPage";
--- src/components/User/Navbar.tsx
+++ src/components/User/Navbar.tsx
@@ -1,11 +1,9 @@
 import { IoLogOut } from "react-icons/io5";
 import useUserStore from "../../store/useUserStore";
 import { toast } from 'react-hot-toast';
-import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
     const clearUserState = useUserStore(state => state.clearUser);
-    const navigate = useNavigate()
 
     const handleLogout = () => {
         clearUserState();
