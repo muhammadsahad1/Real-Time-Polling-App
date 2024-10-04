62b63722ca4b69799dc0182a80fd8e40360fba47
muhammad sahad <muhammadsahad2022@gmail.com>
2024-10-04 06:56:00 +0530

    chore: console.log

--- src/context/SocketContext.tsx
+++ src/context/SocketContext.tsx
@@ -3,14 +3,14 @@ import { io, Socket } from "socket.io-client";
 
 
 const SocketContext = createContext<Socket | null>(null)
-
 export const useSocket = () => {
     return useContext(SocketContext);
 };
 
 export const SocketProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const [socket, setSocket] = useState<Socket | null>(null)
-
+    console.log("url =>",import.meta.env.VITE_BASE_URL);
+    
     useEffect(() => {
         const newSocket = io(import.meta.env.VITE_BASE_URL.replace('/api/', ''))
         setSocket(newSocket)
